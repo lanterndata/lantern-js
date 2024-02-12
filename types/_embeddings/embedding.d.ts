31f6234caec48da3da88210af5a18eb0d7a52bf9
Nairi Harutyunyan <nairi.haroutiounian@gmail.com>
2024-02-12 23:53:59 +0400

    Correct types

--- types/_embeddings/embedding.d.ts
+++
@@ -1,3 +0,0 @@
-import { TextEmbeddingModels, ImageEmbeddingModels, OpenAITextEmbeddingModels, CohereTextEmbeddingModels } from './enums';
-
-export { TextEmbeddingModels, ImageEmbeddingModels, OpenAITextEmbeddingModels, CohereTextEmbeddingModels };
