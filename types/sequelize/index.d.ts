8c2b6b832319c73822b879311bb5b1e491e507fd
Nairi Harutyunyan <nairi.haroutiounian@gmail.com>
2024-02-13 22:02:19 +0400

    Declare TS modules for knex and sequelize

--- types/sequelize/index.d.ts
+++ types/sequelize/index.d.ts
@@ -1,5 +1,20 @@
-import { fromSql, toSql } from '../../src/_common/utils/sql';
+export * from '../_common/utils/sql';
+import { TextEmbeddingModels, ImageEmbeddingModels } from '../_embeddings';
 
-function extend(sequelize: any): void;
+export function extend(sequelize: Sequelize): void;
 
-export { fromSql, toSql, extend };
+declare module 'sequelize' {
+  interface Sequelize {
+    l2Distance: (column: string, vector: number[] | string) => any;
+    cosineDistance: (column: string, vector: number[] | string) => any;
+    hammingDistance: (column: string, vector: number[] | string) => any;
+
+    textEmbedding: (modelKey: TextEmbeddingModels, value: string) => any;
+    imageEmbedding: (modelKey: ImageEmbeddingModels, value: string) => any;
+    generateTextEmbedding: (modelKey: TextEmbeddingModels, value: string) => Promise<any>;
+    generateImageEmbedding: (modelKey: ImageEmbeddingModels, value: string) => Promise<any>;
+
+    createLanternExtension: () => Promise<void>;
+    createLanternExtrasExtension: () => Promise<void>;
+  }
+}
