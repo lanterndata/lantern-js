a9909a752c97319efd0d92a75643606d9201d66d
Nairi Harutyunyan <nairi.haroutiounian@gmail.com>
2024-02-13 20:09:40 +0200

    Declare TS modules for knex and sequelize (#17)
    * Add npmignore and release ga yml config
    
    * Update package version
    
    * Add release.yml github actions configuration file (#13)
    
    * Add npmignore and release ga yml config
    
    * Update package version
    
    * Update release.yml
    
    * Correct types
    
    * Update package version
    
    * Declare TS modules for knex and sequelize
    
    * Update package version

--- types/knex/index.d.ts
+++ types/knex/index.d.ts
@@ -1,3 +1,21 @@
-import { fromSql, toSql } from '../_common/utils/sql';
+export * from '../_common/utils/sql';
+import { TextEmbeddingModels, ImageEmbeddingModels } from '../_embeddings';
 
-export { fromSql, toSql };
+declare module 'knex' {
+  interface Knex {
+    l2Distance: (column: string, vector: number[] | string) => any;
+    cosineDistance: (column: string, vector: number[] | string) => any;
+    hammingDistance: (column: string, vector: number[] | string) => any;
+
+    textEmbedding: (modelKey: TextEmbeddingModels, value: string) => any;
+    imageEmbedding: (modelKey: ImageEmbeddingModels, value: string) => any;
+    generateTextEmbedding: (modelKey: TextEmbeddingModels, value: string) => Promise<any>;
+    generateImageEmbedding: (modelKey: ImageEmbeddingModels, value: string) => Promise<any>;
+  }
+  namespace Knex {
+    interface SchemaBuilder {
+      createLanternExtension: () => Promise<void>;
+      createLanternExtrasExtension: () => Promise<void>;
+    }
+  }
+}
