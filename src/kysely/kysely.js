d38c6dfd726fbd69e8f6b84eaece5d26cf2c28bc
Nairi Harutyunyan <nairi.haroutiounian@gmail.com>
2024-02-25 02:45:03 +0400

    Add kysely TS config

--- src/kysely/kysely.js
+++ src/kysely/kysely.js
@@ -1,47 +1,47 @@
 const { fromSql, toSql } = require('../_common/utils/sql');
 const { getTextEmbeddingModelName, getImageEmbeddingModelName } = require('../_embeddings/models');
 
-function extend({ Kysely, sql }) {
-  Kysely.createLanternExtension = function () {
+function extend(sql) {
+  sql.createLanternExtension = function () {
     return sql`CREATE EXTENSION IF NOT EXISTS lantern`;
   };
 
-  Kysely.createLanternExtrasExtension = function () {
+  sql.createLanternExtrasExtension = function () {
     return sql`CREATE EXTENSION IF NOT EXISTS lantern_extras`;
   };
 
   // embedding generation methods
-  Kysely.generateTextEmbedding = function (modelKey, value) {
+  sql.generateTextEmbedding = function (modelKey, value) {
     const modelName = getTextEmbeddingModelName(modelKey);
     return sql`SELECT text_embedding(${modelName}, ${value})`;
   };
 
-  Kysely.generateImageEmbedding = function (modelKey, value) {
+  sql.generateImageEmbedding = function (modelKey, value) {
     const modelName = getImageEmbeddingModelName(modelKey);
     return sql`SELECT image_embedding(${modelName}, ${value})`;
   };
 
   // embedding literals
-  Kysely.textEmbedding = function (modelKey, value) {
+  sql.textEmbedding = function (modelKey, value) {
     const modelName = getTextEmbeddingModelName(modelKey);
     return sql`text_embedding(${modelName}, ${sql.ref(value)})`;
   };
 
-  Kysely.imageEmbedding = function (modelKey, value) {
+  sql.imageEmbedding = function (modelKey, value) {
     const modelName = getImageEmbeddingModelName(modelKey);
     return sql`image_embedding(${modelName}, ${sql.ref(value)})`;
   };
 
   // distance search literals
-  Kysely.l2Distance = function (column, value) {
+  sql.l2Distance = function (column, value) {
     return sql`${sql.ref(column)} <-> ${toSql(value)}`;
   };
 
-  Kysely.cosineDistance = function (column, value) {
+  sql.cosineDistance = function (column, value) {
     return sql`${sql.ref(column)} <=> ${toSql(value)}`;
   };
 
-  Kysely.hammingDistance = function (column, value) {
+  sql.hammingDistance = function (column, value) {
     return sql`${sql.ref(column)} <+> ${toSql(value)}`;
   };
 }
---
+++ types/kysely/index.d.ts
@@ -0,0 +1,22 @@
+import { Kysely, Sql, QueryExecutorProvider } from 'kysely';
+import { TextEmbeddingModelType, ImageEmbeddingModelType } from 'lanterndata/types/_embeddings/model-types';
+
+export * from 'lanterndata/types/_common/utils/sql';
+
+export declare function extend({ Kysely, sql }: { Kysely: Kysely; sql: Sql }): void;
+
+declare module 'kysely' {
+  interface Sql {
+    l2Distance(column: string, vector: number[] | string): RawBuilder;
+    cosineDistance(column: string, vector: number[] | string): RawBuilder;
+    hammingDistance(column: string, vector: number[] | string): RawBuilder;
+
+    textEmbedding(modelKey: TextEmbeddingModelType, value: string): RawBuilder;
+    imageEmbedding(modelKey: ImageEmbeddingModelType, value: string): RawBuilder;
+    generateTextEmbedding(modelKey: TextEmbeddingModelType, value: string): RawBuilder;
+    generateImageEmbedding(modelKey: ImageEmbeddingModelType, value: string): RawBuilder;
+
+    createLanternExtension(): RawBuilder;
+    createLanternExtrasExtension(): RawBuilder;
+  }
+}
