fb561db6f29eb8eb621675380d6f6a65d8076b41
Nairi Harutyunyan <nairi.haroutiounian@gmail.com>
2024-03-08 14:41:21 +0400

    Draft version

--- test/typeorm.test.mjs
+++ test/typeorm.test.mjs
@@ -2,7 +2,11 @@ import assert from 'node:assert';
 
 import { describe, it, after, before } from 'node:test';
 import { DataSource, EntitySchema } from 'typeorm';
-import { createLanternExtension, createLanternExtrasExtension } from 'lanterndata/typeorm';
+import {
+  createLanternExtension, createLanternExtrasExtension,
+  l2Distance, hammingDistance, cosineDistance,
+  generateTextEmbedding, generateImageEmbedding
+} from 'lanterndata/typeorm';
 import { TextEmbeddingModels, ImageEmbeddingModels } from 'lanterndata/embeddings';
 
 import sqlQueries from './_common/sql.mjs';
@@ -14,11 +18,11 @@ const { CLIP_VIT_B_32_VISUAL } = ImageEmbeddingModels;
 
 async function dropTables(AppDataSource) {
   await AppDataSource.query(sqlQueries.books.dropTable);
-  await AppDataSource.query(sqlQueries.books.dropTable);
+  await AppDataSource.query(sqlQueries.movies.dropTable);
 }
 
 describe('Typeorm', () => {
-  let AppDataSource, Book, Movie;
+  let AppDataSource; let Book; let Movie; let bookRepository; let movieRepository;
 
   before(() => {
     Book = new EntitySchema({
@@ -73,85 +77,94 @@ describe('Typeorm', () => {
 
     await AppDataSource.query(createLanternExtension());
     await AppDataSource.query(createLanternExtrasExtension());
+
+    dropTables(AppDataSource);
   });
 
   it('should create a table [REAL] with index and data', async () => {
     await AppDataSource.query(sqlQueries.books.createTable);
 
-    const bookRepository = AppDataSource.getRepository(Book);
+    bookRepository = AppDataSource.getRepository(Book);
 
-    for (let bookItem of newBooks) {
+    for (const bookItem of newBooks) {
       await bookRepository.save(bookItem);
     }
 
     await AppDataSource.query(sqlQueries.books.createIndexDef);
   });
 
-  // it('should create a table [INT] with index and data', async () => {
-  //   await knex.schema.createTable('movies', (table) => {
-  //     table.increments('id');
-  //     table.specificType('embedding', 'INT[]');
-  //   });
+  it('should create a table [INT] with index and data', async () => {
+    await AppDataSource.query(sqlQueries.movies.createTable);
 
-  //   await knex('movies').insert(newMovies);
+    movieRepository = AppDataSource.getRepository(Movie);
 
-  //   knex.schema.table('movies', (table) => {
-  //     table.index(knex.raw('embedding dist_hamming_ops'), 'idx_embedding', 'lantern_hnsw');
-  //   });
-  // });
+    for (const movieItem of newMovies) {
+      await movieRepository.save(movieItem);
+    }
 
-  // it('should find using L2 distance', async () => {
-  //   const books = await knex('books')
-  //     .orderBy(knex.l2Distance('embedding', [1, 1, 1]))
-  //     .limit(5);
+    await AppDataSource.query(sqlQueries.movies.createIndexDef);
+  });
 
-  //   assert.deepStrictEqual(
-  //     books.map((v) => v.id),
-  //     [1, 3, 2, 4],
-  //   );
+  it('should find using L2 distance', async () => {
+    const books = await bookRepository
+      .createQueryBuilder('book')
+      .orderBy(l2Distance('embedding', ':myEmbedding'))
+      .setParameters({ myEmbedding: [1, 1, 1] })
+      .limit(5)
+      .getMany();
 
-  //   assert.deepStrictEqual(books[0].embedding, [1, 1, 1]);
-  //   assert.deepStrictEqual(books[1].embedding, [1, 1, 2]);
-  //   assert.deepStrictEqual(books[2].embedding, [2, 2, 2]);
-  // });
+    assert.deepStrictEqual(
+      books.map((v) => v.id),
+      [1, 3, 2, 4],
+    );
 
-  // it('should find using Cosine distance', async () => {
-  //   const books = await knex('books')
-  //     .orderBy(knex.cosineDistance('embedding', [1, 1, 1]))
-  //     .limit(5);
+    assert.deepStrictEqual(books[0].embedding, [1, 1, 1]);
+    assert.deepStrictEqual(books[1].embedding, [1, 1, 2]);
+    assert.deepStrictEqual(books[2].embedding, [2, 2, 2]);
+  });
 
-  //   assert.deepStrictEqual(
-  //     books.map((v) => v.id),
-  //     [1, 2, 3, 4],
-  //   );
-  // });
+  it('should find using Cosine distance', async () => {
+    const books = await bookRepository
+      .createQueryBuilder('book')
+      .orderBy(cosineDistance('embedding', ':myEmbedding'))
+      .setParameters({ myEmbedding: [1, 1, 1] })
+      .limit(5)
+      .getMany();
 
-  // it('should find using Hamming distance', async () => {
-  //   const movies = await knex('movies')
-  //     .orderBy(knex.hammingDistance('embedding', [1, 1, 1]))
-  //     .limit(5);
+    assert.deepStrictEqual(
+      books.map((v) => v.id),
+      [1, 2, 3, 4],
+    );
+  });
 
-  //   assert.deepStrictEqual(
-  //     movies.map((v) => v.id),
-  //     [1, 3, 2, 4],
-  //   );
-  // });
+  it('should find using Hamming distance', async () => {
+    const movies = await movieRepository
+      .createQueryBuilder('movies')
+      .orderBy(hammingDistance('embedding', ':myEmbedding'))
+      .setParameters({ myEmbedding: [1, 1, 1] })
+      .limit(5)
+      .getMany();
 
-  // it('should fail because of wrong embedding dimensions', async () => {
-  //   await knex('books')
-  //     .insert({ embedding: [1] })
-  //     .catch((e) => assert.equal(e.message.includes('Wrong number of dimensions: 1 instead of 3 expected'), true));
-  // });
+    assert.deepStrictEqual(
+      movies.map((v) => v.id),
+      [1, 3, 2, 4],
+    );
+  });
 
-  // it('should create simple text embedding', async () => {
-  //   const result = await knex.generateTextEmbedding(BAAI_BGE_BASE_EN, 'hello world');
-  //   assert.equal(result.rows[0].text_embedding.length, 768);
-  // });
+  it('should fail because of wrong embedding dimensions', async () => {
+    await bookRepository.save({ embedding: [1] })
+      .catch((e) => assert.equal(e.message.includes('Wrong number of dimensions: 1 instead of 3 expected'), true));
+  });
 
-  // it('should create simple image embedding', async () => {
-  //   const result = await knex.generateImageEmbedding(CLIP_VIT_B_32_VISUAL, imageUrl);
-  //   assert.equal(result.rows[0].image_embedding.length, 512);
-  // });
+  it('should create simple text embedding', async () => {
+    const result = await AppDataSource.query(generateTextEmbedding(BAAI_BGE_BASE_EN), [ 'hello world' ]);
+    assert.equal(result[0].text_embedding.length, 768);
+  });
+
+  it('should create simple image embedding', async () => {
+    const result = await AppDataSource.query(generateImageEmbedding(CLIP_VIT_B_32_VISUAL), [ imageUrl ]);
+    assert.equal(result[0].image_embedding.length, 512);
+  });
 
   // it('select text embedding based on book names in the table', async () => {
   //   const selectLiteral = knex.textEmbedding(BAAI_BGE_BASE_EN, 'name');
